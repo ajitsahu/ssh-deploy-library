0fe8035184db060dc7ebb120bdcf6971d9a3b565
Ajit Sahu <ajitsahu69@gmail.com>
2019-02-12 08:55:59 +0900

    Update sshDeploy.groovy
    added 'r_group_1'

--- vars/sshDeploy.groovy
+++ vars/sshDeploy.groovy
@@ -4,7 +4,7 @@ def call(String yamlName) {
     withCredentials([usernamePassword(credentialsId: yaml.config.credentials_id, passwordVariable: 'password', usernameVariable: 'userName')]) {
         yaml.steps.each { stageName, step ->
             step.each {
-                def remoteGroups = [:]
+                def remoteGroups = [r_group_1]
                 def allRemotes = []
                 it.remote_groups.each {
                     remoteGroups[it] = yaml.remotes."$it"
