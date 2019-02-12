a0932562f2566d5776d79f6f4cea25dd0e117cb0
Ajit Sahu <ajitsahu69@gmail.com>
2019-02-12 16:54:09 +0900

    Update sshDeploy.groovy

--- vars/sshDeploy.groovy
+++ vars/sshDeploy.groovy
@@ -5,7 +5,6 @@ def call(String yamlName) {
     withCredentials([usernamePassword(credentialsId: yaml.config.credentials_id, passwordVariable: 'password', usernameVariable: 'userName')]) {
         yaml.steps.each { stageName, step ->
             step.each {
-                def remoteGroups = [:]
                 def allRemotes = []
                 def commandGroups = [:]
                 it.command_groups.each {
