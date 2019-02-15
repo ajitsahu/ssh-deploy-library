6ae70dfca9de38cd14e586a4800ff8bb3d525a7e
Ajit Sahu <ajitsahu69@gmail.com>
2019-02-16 00:20:46 +0900

    Update sshDeploy.groovy

--- vars/sshDeploy.groovy
+++ vars/sshDeploy.groovy
@@ -1,12 +1,13 @@
 #!/usr/bin/env groovy
 
 def call(String yamlName) {
-    sshDeploy(yaml)
+    sshDeploy(yamlName, false)
 }
 
-def call(boolean dryRun) {
-    sshDeploy(dryRun)
+def call(String yamlName, boolean dryRun) {
+    sshDeploy(yamlName, dryRun)
 }
+
 def call(yaml, boolean dryRun) {
     if(!yaml.config)
         error "config missing in the given yml file."
