71613f250cf3a7740a60c71c687f4e27e2d38842
Ajit Sahu <ajitsahu69@gmail.com>
2019-02-24 19:28:50 +0900

    Update sshDeploy.groovy

--- vars/sshDeploy.groovy
+++ vars/sshDeploy.groovy
@@ -172,7 +172,7 @@ private executeCommands(remote, stageName, remoteGroupName, commandGroupName, co
             sshGet remote: remote, from: command.from, into: command.into, override: command.override
             break
         case "puts":
-            sshPut remote: remote, from: command, into: command
+            sshPut remote: remote, from: command.from, into: command.into
             break
         case "removes":
             sshRemove remote: remote, path: command
