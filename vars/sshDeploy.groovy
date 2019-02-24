b9fe64c5b2081336cbe9e1b4eb28c8181551118e
Ajit Sahu <ajitsahu69@gmail.com>
2019-02-24 14:59:12 +0900

    Update sshDeploy.groovy

--- vars/sshDeploy.groovy
+++ vars/sshDeploy.groovy
@@ -3,25 +3,50 @@
 def call(String yamlName) {
     def yaml = readYaml file: yamlName
     withCredentials([usernamePassword(credentialsId: yaml.config.credentials_id, passwordVariable: 'password', usernameVariable: 'userName')]) {
+        
+        if(!userName && params.SSH_USER) {
+            error "userName is null or empty, please check credentials_id."
+        }
+
+        if(!password && params.PASSWORD) {
+            error "password is null or empty, please check credentials_id."
+        }
+        
         yaml.steps.each { stageName, step ->
             step.each {
                 def remoteGroups = [:]
                 def allRemotes = []
+                
                 it.remote_groups.each {
+                    if(!yaml.remote_groups."$it") {
+                        error "remotes groups are empty/invalid for the given stage: ${stageName}, command group: ${it}. Please check yml."
+                    }
                     remoteGroups[it] = yaml.remote_groups."$it"
                 }
 
                 def commandGroups = [:]
                 it.command_groups.each {
+                    if(!yaml.command_groups."$it") {
+                        error "command groups are empty/invalid for the given stage: ${stageName}, command group: ${it}. Please check yml."
+                    }
                     commandGroups[it] = yaml.command_groups."$it"
                 }
-                def isSudo = false
+                def isSudo = true
                 remoteGroups.each { remoteGroupName, remotes ->
                     allRemotes += remotes.collect { remote ->
+                        if(!remote.host) {
+                            throw IllegalArgumentException("host missing for one of the nodes in ${remoteGroupName}")
+                        }
                         if(!remote.name)
                             remote.name = remote.host
-                        remote.user = userName
-                        remote.password = password
+                        if(params.SSH_USER) {
+                            remote.user = params.SSH_USER
+                            remote.password = params.PASSWORD
+                            isSudo = true
+                        } else {
+                            remote.user = userName
+                            remote.password = password
+                        }
                         remote.allowAnyHosts = true
                         remote.groupName = remoteGroupName
                         remote
@@ -30,15 +55,15 @@ def call(String yamlName) {
                 if(allRemotes) {
                     if(allRemotes.size() > 1) {
                         def stepsForParallel = allRemotes.collectEntries { remote ->
-                            ["${remote.groupName}-${remote.name}" : transformIntoStep(stageName, remote.groupName, remote, commandGroups)]
+                            ["${remote.groupName}-${remote.name}" : transformIntoStep(stageName, remote.groupName, remote, commandGroups, isSudo, yaml.config)]
                         }
-                        stage(stageName) {
+                        stage(stageName + " \u2609 Size: ${allRemotes.size()}") {
                             parallel stepsForParallel
                         }
                     } else {
                         def remote = allRemotes.first()
                         stage(stageName + "\n" + remote.groupName + "-" + remote.name) {
-                            transformIntoStep(stageName, remote.groupName, remote, commandGroups).call()
+                            transformIntoStep(stageName, remote.groupName, remote, commandGroups, isSudo, yaml.config).call()
                         }
                     }
                 }
@@ -46,6 +71,7 @@ def call(String yamlName) {
         }
     }
 }
+
 private transformIntoStep(stageName, remoteGroupName, remote, commandGroups, isSudo, config) {
     return {
         def finalRetryResult = true
@@ -55,7 +81,11 @@ private transformIntoStep(stageName, remoteGroupName, remote, commandGroups, isS
                 command.each { commandName, commandList ->
                     commandList.each {
                         validateCommands(stageName, remoteGroupName, commandGroupName, commandName, it)
-                    }
+                        executeCommands(remote, stageName, remoteGroupName, commandGroupName, commandName, it, isSudo)
+                            echo "DryRun Mode: Running ${commandName}."
+                            echo "Remote: ${remote}"
+                            echo "Command: ${it}"
+                     }
                 }
             }
         }
@@ -66,7 +96,7 @@ private validateCommands(stageName, remoteGroupName, commandGroupName, commandNa
     if(commandName in ["gets", "puts"]) {
         if(!command.from)
             error "${stageName} -> ${remoteGroupName} -> ${commandGroupName} -> ${commandName} -> from is empty or null."
-         if(!command.into)
+        if(!command.into)
             error "${stageName} -> ${remoteGroupName} -> ${commandGroupName} -> ${commandName} -> into is empty or null."
     }
 }
@@ -77,13 +107,13 @@ private executeCommands(remote, stageName, remoteGroupName, commandGroupName, co
             sshCommand remote: remote, command: command, sudo: isSudo
             break
         case "scripts":
-            sshScript remote: remote, script: command
+            sshScript remote: remote, script: command, sudo: isSudo
             break
         case "gets":
             sshGet remote: remote, from: command.from, into: command.into, override: command.override
             break
         case "puts":
-            sshPut remote: remote, from: from, into: into
+            sshPut remote: remote, from: command.from, into: command.into
             break
         case "removes":
             sshRemove remote: remote, path: command
